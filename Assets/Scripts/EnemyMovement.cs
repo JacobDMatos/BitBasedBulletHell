c6180252d4001bce8804ffa8c8b4a60c5d8ab387
JacobMatos <106333846+JacobDMatos@users.noreply.github.com>
2023-06-30 13:19:45 -0400

    Project files

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public GameObject enemy;
+    public AudioClip deathSFX;
+    public GameObject gameOver;
+
+    private float z;
+    private float x;
+    private Vector3 dir;
+    private Vector3 vel;
+    private float closeDelay;
+    private bool isPlayerAlive = true;
+    private bool isMoving = false;
+
+    void Start()
+    {
+        gameOver.SetActive(false);
+        InvokeRepeating("MilestoneCheck", 30.0f, 30.0f);
+    }
+
+    void Update()
+    {
+        if (!isMoving)
+        {
+            if (Time.time >= 2.0f)
+            {
+                GetComponent<Rigidbody>().velocity = new Vector3(speed, 0, speed);
+                isMoving = true;
+            }
+        }
+
+        if (Time.time >= 90.0f)
+        {
+            CancelInvoke("MilestoneCheck");
+        }
+
+        if (!isPlayerAlive)
+        {
+            closeDelay += Time.deltaTime;
+            if (closeDelay > 5.0f)
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        vel = GetComponent<Rigidbody>().velocity;
+
+        if (other.gameObject.CompareTag("LeftWall"))
+        {
+            z = vel.z;
+            dir = new Vector3(1, 0, z).normalized;
+            GetComponent<Rigidbody>().velocity = dir * speed;
+        } else if (other.gameObject.CompareTag("RightWall"))
+        {
+            z = vel.z;
+            dir = new Vector3(-1, 0, z).normalized;
+            GetComponent<Rigidbody>().velocity = dir * speed;
+        } else if (other.gameObject.CompareTag("TopWall"))
+        {
+            x = vel.x;
+            dir = new Vector3(x, 0, -1).normalized;
+            GetComponent<Rigidbody>().velocity = dir * speed;
+        } else if (other.gameObject.CompareTag("BottomWall"))
+        {
+            x = vel.x;
+            dir = new Vector3(x, 0, 1).normalized;
+            GetComponent<Rigidbody>().velocity = dir * speed;
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            AudioSource.PlayClipAtPoint(deathSFX, new Vector3(5, 10, 5));
+            gameOver.SetActive(true);
+            isPlayerAlive = false;
+        }
+    }
+
+    void MilestoneCheck()
+    {
+        Instantiate(enemy, new Vector3(Random.Range(1.0f, 9.0f), 0.25f, Random.Range(1.0f, 9.0f)), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
+	public TextMeshProUGUI scoreText;
+	public TextMeshProUGUI timer;
+	public AudioClip pickupSFX;
+
+	private float seconds;
+	private float minutes;
+	private int score = 0;
+	private int row;
+	private int column;
+
+    void Start()
+    {
+		
+    }
+
+	void Update()
+    {
+		minutes = (int)(Time.time / 60.0f);
+		seconds = (int)(Time.time % 60.0f);
+		timer.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+	}
+
+	void FixedUpdate()
+	{
+		float hor = 0;
+		float ver = 0;
+		if (Input.GetKey("a"))
+			hor = -1;
+		if (Input.GetKey("d"))
+			hor = 1;
+		if (Input.GetKey("w"))
+			ver = 1;
+		if (Input.GetKey("s"))
+			ver = -1;
+		Vector3 vel = new Vector3(hor, 0, ver);
+		GetComponent<Rigidbody>().position += vel * speed * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other)
+    {
+		if (other.gameObject.CompareTag("Pickup"))
+		{
+			other.gameObject.SetActive(false);
+			AudioSource.PlayClipAtPoint(pickupSFX, new Vector3(5, 10, 5));
+			score++;
+			SetScore();
+			SetPickupSpawn(other);
+			other.gameObject.SetActive(true);
+		}
+	}
+
+	void SetScore()
+    {
+		scoreText.text = "Score: " + score.ToString();
+    }
+
+	void SetPickupSpawn(Collider other)
+    {
+		System.Random r = new System.Random();
+		row = r.Next(0, 8);
+		column = r.Next(0, 8);
+		other.transform.position = new Vector3(row + 1, 0.125f, column + 1);
+    }
+}
